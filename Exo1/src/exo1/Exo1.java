36eea583717807ab3f7be6c20c42a22bbb802ee5
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-09-17 12:42:59 +0200

    TP0

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,30 @@
+/*
+ * TPO Exercice 1
+ * VABRE
+ * Septembre 17
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alien
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("bonjour");
+        System.out.println("au revoir");
+        
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+prenom = sc.nextLine();
+    }
+    
+}
