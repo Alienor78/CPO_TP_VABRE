dbc6ab6f4df6a68035586f3248887f9dfe034213
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-09-20 17:17:08 +0200

    TP0

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -4,7 +4,7 @@
  * Sebtembre 17
  */
 package exo2;
-
+import java.util.Scanner;
 /**
  *
  * @author alien
@@ -15,20 +15,24 @@ public class Exo2 {
      * @param args the command line arguments
      */
     
+
     public static void main(String[] args) {
 //Declaration des variables
 int nb; // nombre dentiers a additionner
 int result; // resultat
 int ind; //indice
-nb=5;
 result=0;
+Scanner sc = new Scanner(System.in);
+System.out.println("\n Entrer le nombre :");
+nb = sc.nextInt(); 
 // Addition des nb premiers entiers
 ind=1;
 while (ind <= nb) {
 result=result+ind;
+ind+=1;
 }
 // Affichage du resultat
-System.out.println();
-System.out.println("La somme des "+ nb + "entiers est: "+result);
-}
+    System.out.println();
+    System.out.println("La somme des "+ nb + " entiers est: "+result);
+    }
 }
