7fcd177411f58873c355b8352886f9d79499c49d
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-09-23 12:59:39 +0200

    TP1

---
+++ TP1_guessMyNumber_VABRE/src/tp1_guessmynumber_vabre/TP1_guessMyNumber_VABRE.java
@@ -0,0 +1,22 @@
+/*
+ * TDB
+ * VABRE Aliénor
+ * 23   Septembre
+ */
+package tp1_guessmynumber_vabre;
+
+/**
+ *
+ * @author alien
+ */
+public class TP1_guessMyNumber_VABRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random generateurAleat = new Random();
+        int n = generateyrAleat.nextInt(100);
+    }
+    
+}
