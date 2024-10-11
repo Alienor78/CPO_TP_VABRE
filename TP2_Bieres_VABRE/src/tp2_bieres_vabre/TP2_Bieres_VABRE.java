970dfdf9dd8f02663029fc1be27490973a4ae95b
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-11 09:32:01 +0200

    TP2

--- TP2_Bieres_VABRE/src/tp2_bieres_vabre/TP2_Bieres_VABRE.java
+++ TP2_Bieres_VABRE/src/tp2_bieres_vabre/TP2_Bieres_VABRE.java
@@ -15,10 +15,21 @@ public class TP2_Bieres_VABRE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere();
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0 ,"Dubuisson") ;
+        uneBiere.lireEtiquette();
         
-            
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
+        autreBiere.lireEtiquette();
+        
+        BouteilleBiere TroisiemeBiere = new BouteilleBiere("Goudale", 7.2 , "Brasserie Goudale");
+        TroisiemeBiere.lireEtiquette();
+        
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Desperados", 5.9 , "Alken-Maes") ;
+        quatriemeBiere.lireEtiquette();
+        
+        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Tripel Karmeliet",9.2, "Bosteels" );
+        cinquiemeBiere.lireEtiquette();
         }
     }
     
-}
+
