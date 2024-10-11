1224fa5ef5672529d90cfa902b6c6e1415a05d06
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-11 08:29:49 +0200

    TP2

---
+++ TP2_Bieres_VABRE/src/tp2_bieres_vabre/TP2_Bieres_VABRE.java
@@ -0,0 +1,24 @@
+/*
+ * VABRE
+ * Aliénor
+ * 11/10/2024
+ */
+package tp2_bieres_vabre;
+
+/**
+ *
+ * @author alien
+ */
+public class TP2_Bieres_VABRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere();
+        
+            
+        }
+    }
+    
+}
