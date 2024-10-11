4a7faaedf0a74fb3c7e545fbaef31c96846e3502
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-11 08:30:07 +0200

    TP2

---
+++ TP2_Bieres_VABRE/src/tp2_bieres_vabre/BouteilleBiere.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_vabre;
+
+/**
+ *
+ * @author alien
+ */
+public class BouteilleBiere {
+    String Nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    
+public void lireEtiquette() {
+    System.out.println("Bouteille de " + nom +" \n" + degreAlcool + " degres " + "\n" + " Brasserie : " + brasserie ) ;
+
+}
+      
+}
