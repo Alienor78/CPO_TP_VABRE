17c90ec26a2e6dcd457e66685a60277c8cf886a6
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-14 21:08:23 +0200

    TP2

--- TP2_Bieres_VABRE/src/tp2_bieres_vabre/BouteilleBiere.java
+++ TP2_Bieres_VABRE/src/tp2_bieres_vabre/BouteilleBiere.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * VABRE
+ * Aliénor
+ * 11/10/2024
  */
 package tp2_bieres_vabre;
 
@@ -27,10 +28,10 @@ public class BouteilleBiere{
         System.out.println("Bouteille de " + nom +" \n" + degreAlcool + " degres " + "\n" + "Brasserie : " + brasserie ) ;
 
 }
-    public void decapsuler(){
+    public boolean decapsuler(){
         if (!ouverte){
             ouverte = true;
-            System.out.println("La biere" + nom + "est maintenant ouverte.");
+            System.out.println("La biere" + nom + " est maintenant ouverte.");
         return true;
         }
         else{
@@ -38,6 +39,17 @@ public class BouteilleBiere{
         return false;
         }
     }
-    
+    @Override
+    public String toString() {
+    String chaine_a_retourner;
+    chaine_a_retourner = nom + " (" + degreAlcool + " degrés )" + " est ouverte ? : " ;
+    if (ouverte == true ){
+        chaine_a_retourner += "oui" ;
+    }
+    else{
+        chaine_a_retourner += "non" ;
+    }
+    return chaine_a_retourner ;
+}
     
 }
