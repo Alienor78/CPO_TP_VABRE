750f6aede70e0628e0bc63ffcb4e3a843bf845e1
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-15 12:01:53 +0200

    TP2

---
+++ TP2_relation_1_VABRE/src/tp2_relation_1_vabre/TP2_relation_1_VABRE.java
@@ -0,0 +1,28 @@
+/*
+ * TP2
+ * Aliénor VABRE
+ * 15/10/2024
+ */
+package tp2_relation_1_vabre;
+
+/**
+ *
+ * @author alien
+ */
+public class TP2_relation_1_VABRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        Personne bob = new Personne("Bobby", "Sixkiller");
+        Personne reno = new Personne("Reno", "Raines");
+        System.out.println("liste des voitures disponibles "+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+
+    }
+    
+}
