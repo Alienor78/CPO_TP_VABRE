5a93287b6d0bde55d4943d54041d283d1b148cab
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-15 11:34:15 +0200

    TP2

---
+++ TP2_manip_VABRE/src/tp2_manip_vabre/Moussaka.java
@@ -0,0 +1,18 @@
+package tp2_manip_vabre;
+
+/*
+ * TP2
+ * Aliénor VABRE
+ * 15/10/2024
+ */
+
+/**
+ *
+ * @author alien
+ */
+public class Moussaka {
+    int nbcalories;
+    public Moussaka(int a){
+        this.nbcalories = a;
+    }
+}
