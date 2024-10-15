7309c0ace93282c0a81922b9464211d67976afa0
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-15 11:34:04 +0200

    TP2

---
+++ TP2_manip_VABRE/src/tp2_manip_vabre/Tartiflette.java
@@ -0,0 +1,19 @@
+package tp2_manip_vabre;
+
+/*
+ * TP2
+ * Aliénor VABRE
+ * 15/10/2024
+ */
+
+/**
+ *
+ * @author alien
+ */
+public class Tartiflette {
+    int nbcalories;
+   
+    public Tartiflette(int a){
+        this.nbcalories = a;
+    }
+}
