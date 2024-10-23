9e9f4768ba6ac325c69ccf644362727f07ab6166
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-23 10:47:22 +0200

    TP3

---
+++ TP3_Heroic_Fantasy_VABRE/src/tp3_heroic_fantasy_vabre/Epee.java
@@ -0,0 +1,23 @@
+/*
+ * TP2
+ * Aliénor VABRE
+ * 15/10/2024
+ */
+package tp3_heroic_fantasy_vabre;
+
+/**
+ *
+ * @author alien
+ */
+public class Epee extends Arme{
+    int indiceFinesse;
+
+    public Epee(int indiceFinesse, String nom , int niveau) {
+        super(nom, niveau);
+        this.indiceFinesse = indiceFinesse;
+    }
+   
+    }
+    
+    
+}
