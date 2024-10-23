04eacc0f4692e19a06ce935bb82ee0242aedd89f
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-10-23 10:46:53 +0200

    TP3

---
+++ TP3_Heroic_Fantasy_VABRE/src/tp3_heroic_fantasy_vabre/TP3_Heroic_Fantasy_VABRE.java
@@ -0,0 +1,19 @@
+/*
+c
+ */
+package tp3_heroic_fantasy_vabre;
+
+/**
+ *
+ * @author alien
+ */
+public class TP3_Heroic_Fantasy_VABRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
