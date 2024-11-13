b186cad5a9b2052357e84e67d80c89f83f91a3d0
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-11-13 16:21:09 +0100

    TP3

--- TP3_Heroic_Fantasy_VABRE/src/tp3_heroic_fantasy_vabre/TP3_Heroic_Fantasy_VABRE.java
+++ TP3_Heroic_Fantasy_VABRE/src/tp3_heroic_fantasy_vabre/TP3_Heroic_Fantasy_VABRE.java
@@ -1,7 +1,11 @@
 /*
-c
+ * TP3
+ * Aliénor VABRE
+ * 15/10/2024
  */
 package tp3_heroic_fantasy_vabre;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -13,7 +17,22 @@ public class TP3_Heroic_Fantasy_VABRE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Epee epee1 = new Epee("Excalibur", 7 , 5);
+        Epee epee2 = new Epee("Durandal", 4 , 7);
+        
+        Baton baton1 = new Baton("Chêne", 4, 5);
+        Baton baton2 = new Baton("Charme", 5, 4);
+        
+        
+        ArrayList<Arme> A = new ArrayList<Arme>();
+        A.add(epee1);
+        A.add(epee2);
+        A.add(baton1);
+        A.add(baton2);
+        
+        System.out.println(A.size());
+        System.out.println(A.get(1));
+        
     }
     
 }
