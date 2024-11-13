6100d988a51dce088f1a46fa8565affcc647d2be
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-11-13 16:23:18 +0100

    TP3

---
+++ TP3_Heroic_Fantasy_VABRE/src/Armes/Arme.java
@@ -0,0 +1,30 @@
+/*
+ * TP3
+ * Aliénor VABRE
+ * 15/10/2024
+ */
+package Armes;
+
+/**
+ *
+ * @author alien
+ */
+public abstract class Arme {
+    String nom;
+    int niveau = 0;
+    
+    public Arme( String nom, int niveau){
+        this.nom = nom;
+        
+        if (niveau < 0) {
+            
+        }else{
+        this.niveau = niveau;
+        }
+    }
+    @Override
+    public String toString (){
+        String arme;
+        return arme = "l'arme est " + nom + " et est de niveau " + niveau;
+    }
+}
